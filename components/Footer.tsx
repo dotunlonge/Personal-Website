69337f6caecc35205713d8df041fb43a4b1efd1b
Dotun Longe <oludotunlonge@gmail.com>
2022-11-09 18:31:38 +0100

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,12 @@ export default function Footer() {
     const year = new Date().getFullYear();
 
     return (
-        <footer className="dark-bg flex" style={{ textAlign: "center", margin: "0 auto 2em", padding: 0 }}>
+        <footer className="dark-bg flex" style={{
+          textAlign: "center",
+          margin: "0 auto 2em",
+          padding: '0rem 2rem 0rem 2rem',
+          lineHeight: '1.5em', fontSize: '0.7em'
+      }}>
             <span style={{ textAlign: 'center', margin: 'auto', fontSize: "14px" }} className="white">
               Copyright © {year} <Link href="/">Oludotun Longe</Link> - All Rights Reserved.
             </span>
