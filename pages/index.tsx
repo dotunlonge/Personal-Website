1d090e6a0a8bb2e680e92fd4785007f976da0328
Dotun Longe <oludotunlonge@gmail.com>
2022-11-09 17:10:03 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,31 +8,27 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Blaiti - Frontend Developer</title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="author" content="Skander Blaiti" />
-        <meta name="keywords" content="ReactJS, NextJS" />
-        <meta name="description" content="I am a talented Front-End developer with a UI/UX design background." />
+        <title>Oludotun Longe - Full-Stack JavaScript Software Engineer</title>
+        <link rel="icon" href="/Dotun2.svg" />
+        <meta name="author" content="Oludotun Longe" />
+        <meta name="keywords" content="ReactJS, NextJS, Tailwing, JavaScript, NodeJS, AWS" />
+        <meta name="description" content="I am a talented Full-Stack JavaScript Software Engineer with a UI/UX design background." />
         <meta charSet="UTF-8" />
         <meta name="theme-color" content="#2D2E32" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
         <meta property="og:site_name" content="Blaiti - Frontend Developer" />
         <meta property="og:locale" content="en_GB" />
-        <title data-rh="true">Blaiti - Frontend Developer</title>
+        <title data-rh="true">Oludotun Longe - Full-Stack JavaScript Software Engineer</title>
         <meta data-rh="true" property="og:type" content="website"/>
-        <meta data-rh="true" property="og:title" content="Blaiti - Frontend Developer"/>
-        <meta data-rh="true" property="og:image" content="/images/blaiti.png"/>
+        <meta data-rh="true" property="og:title" content="Oludotun Longe - Full-Stack JavaScript Software Engineer"/>
+        <meta data-rh="true" property="og:image" content="/images/oludotunlonge.jpg"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Archivo:wght@200;300;400;500;600&display=swap" rel="stylesheet"/>
       </Head>
 
       {/* Header */}
       <Header />
-
-      <main>
-        {/* About */}
-        <About />
-      </main>
-
       {/* Footer */}
       <Footer />
     </>
