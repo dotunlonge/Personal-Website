3d21d6fa49a169524d6095981154074a4a6803d8
Dotun Longe <oludotunlonge@gmail.com>
2024-03-07 13:11:12 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,19 +8,20 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Oludotun Longe - Full-Stack JavaScript Software Engineer</title>
+        <title>Oludotun Longe - Expert Full-Stack Software Engineer & Innovator in Web3 and AI Technologies</title>
         <link rel="icon" href="/Dotun2.svg" />
         <meta name="author" content="Oludotun Longe" />
-        <meta name="keywords" content="ReactJS, NextJS, Tailwing, JavaScript, NodeJS, AWS" />
-        <meta name="description" content="I am a talented Full-Stack JavaScript Software Engineer with a UI/UX design background." />
+        <meta name="keywords" content='JavaScript, TypeScript, React, Redux, Zustand, React Query, React Native, NextJS, NodeJS,
+ExpressJS, RabbitMQ, Redis, MeteorJS, Docker, Blockchain, APIs (REST and GraphQL), GRPC, Gatsby, Netlify CMS, Wordpress, AWS, MongoDB, Postgres, SQL, Firebase, PHP, Figma, HTML5, SCSS, TailwindCSS, CSS3' />
+        <meta name="description" content="Discover Oludotun Longe, a visionary Full-Stack Developer specializing in JavaScript, AI, ML, and web3 technologies. With over 7 years of experience spanning startups, web development agencies, and freelance projects, Dotun excels in crafting user-centric solutions that drive engagement and innovation in the healthcare and security sectors. Explore dotunlonge.com to learn how Dotun's passion for technology and problem-solving can elevate your next project." />
         <meta charSet="UTF-8" />
         <meta name="theme-color" content="#2D2E32" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta property="og:site_name" content="Blaiti - Frontend Developer" />
+        <meta property="og:site_name" content="Oludotun Longe - Expert Full-Stack Software Engineer & Innovator in Web3 and AI Technologies" />
         <meta property="og:locale" content="en_GB" />
-        <title data-rh="true">Oludotun Longe - Full-Stack JavaScript Software Engineer</title>
+        <title data-rh="true">Oludotun Longe - Expert Full-Stack Software Engineer</title>
         <meta data-rh="true" property="og:type" content="website"/>
-        <meta data-rh="true" property="og:title" content="Oludotun Longe - Full-Stack JavaScript Software Engineer"/>
+        <meta data-rh="true" property="og:title" content="Oludotun Longe - Expert Full-Stack Software Engineer & Innovator in Web3 and AI Technologies"/>
         <meta data-rh="true" property="og:image" content="/images/oludotunlonge.jpg"/>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
